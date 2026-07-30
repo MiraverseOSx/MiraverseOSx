0a7e07348ed78affaf2f5a2d2bffdc8fd5721c71
mirxclej <miraclejerome13@gmail.com>
2026-07-29 20:24:54 -0400

    yay

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the OS menu bar', () => {
-  render(<App />);
-  const brand = screen.getByText(/MiraverseOSx/i);
-  expect(brand).toBeInTheDocument();
-});
